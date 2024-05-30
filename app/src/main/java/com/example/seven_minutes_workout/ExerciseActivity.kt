56c0cc1b9b51c811ad5faa9478924269a0b989fa
Alennikov Boris <70971570+AlenniBoris@users.noreply.github.com>
2024-05-30 20:56:07 +0300

    Added all pictures, created the start page, created exercise page and added the toolbar to it, added viewbinding to the project

---
+++ app/src/main/java/com/example/seven_minutes_workout/ExerciseActivity.kt
@@ -0,0 +1,31 @@
+package com.example.seven_minutes_workout
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.seven_minutes_workout.databinding.ActivityExerciseBinding
+
+class ExerciseActivity : AppCompatActivity() {
+
+    private var binding: ActivityExerciseBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityExerciseBinding.inflate(layoutInflater)
+
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarExercise)
+
+        if (supportActionBar != null){
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+
+        binding?.toolbarExercise?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+}
--- app/src/main/java/com/example/seven_minutes_workout/MainActivity.kt
+++ app/src/main/java/com/example/seven_minutes_workout/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.seven_minutes_workout
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.seven_minutes_workout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding!!.root)
+
+        val flStartButton: FrameLayout = binding!!.flStart
+        flStartButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, ExerciseActivity::class.java)
+            startActivity(intent)
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
     }
 }
